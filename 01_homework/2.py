7aacfe15e7e5ec3eb3fd7513e65d2e540a690923
BreslavetsAlexander <46530223+BreslavetsAlexander@users.noreply.github.com>
2019-09-16 18:21:24 +0300

    2.py

---
+++ 01_homework/2.py
@@ -0,0 +1,10 @@
+a = float(input('a: '))
+b = float(input('b: '))
+c = float(input('c: '))
+d = float(input('d: '))
+f = float(input('f: '))
+
+if f-d == 0:
+    print('Делить на ноль нельзя!')
+else:
+    print((a * b - c) / (f - d))
