3c88115e4f34b0b2827a152a05ea0dded5974af0
BreslavetsAlexander <46530223+BreslavetsAlexander@users.noreply.github.com>
2019-09-16 20:18:54 +0300

    Update 7.py

--- 01_homework/7.py
+++ 01_homework/7.py
@@ -2,8 +2,8 @@ x = int(input('x: '))
 y = int(input('y: '))
 s = 0
 
-for num in range(x, y+1):
-    if num % 5 == 0:
+for num in range(x, y + 1):
+    if not(num % 5):
         s += num
 
 print(s)
