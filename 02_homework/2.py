05a43fa97dfdb2df822e3a6294d375b7bed6d212
BreslavetsAlexander <46530223+BreslavetsAlexander@users.noreply.github.com>
2019-09-27 19:05:19 +0300

    Update 2.py

--- 02_homework/2.py
+++ 02_homework/2.py
@@ -1,13 +1,9 @@
 lst = []
-num = ''
 s = input('s: ')
 
 while s:
     lst.append(s)
     s = input('s: ')
 
-lst = sorted(lst)[::-1]
-for i in lst:
-    num += i
-
-print(int(num))
+lst = ''.join(sorted(lst)[::-1])
+print(int(lst))
