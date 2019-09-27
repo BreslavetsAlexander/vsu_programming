15c4e7f73480c795792eeb77c27515fe5398c58c
BreslavetsAlexander <46530223+BreslavetsAlexander@users.noreply.github.com>
2019-09-27 20:18:04 +0300

    Update 5.py

--- 02_homework/5.py
+++ 02_homework/5.py
@@ -1,7 +1,7 @@
-num = int(input('num: '))
+n = int(input('num: '))
 
-print(int(num / 10000))
-print(int(num / 1000) % 10)
-print(int(num / 100) % 10)
-print(int(num / 10) % 10)
-print(num % 10)
+i = len(str(n))  # работает не только для 5-значного числа
+while i:
+    n = (n % 10**i)
+    i -= 1
+    print(int(n / 10**i))
