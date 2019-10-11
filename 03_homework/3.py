d589cd16c77aad8b670dea64199121915afb40e7
BreslavetsAlexander <46530223+BreslavetsAlexander@users.noreply.github.com>
2019-10-11 18:14:00 +0300

    Update 3.py

--- 03_homework/3.py
+++ 03_homework/3.py
@@ -1,8 +1,8 @@
 def print_feb_nums(n):
     feb_list = [0, 1]
     for i in range(n - 2):
-        last = feb_list[len(feb_list) - 1]
-        penultimate = feb_list[len(feb_list) - 2]
+        last = feb_list[-1]
+        penultimate = feb_list[-2]
         feb_list.append(last + penultimate)
     print(feb_list)
 
