ca806c3cbe834305063ccbc2818e1da54f108a33
BreslavetsAlexander <46530223+BreslavetsAlexander@users.noreply.github.com>
2019-11-03 13:22:42 +0300

    Update hash_table.py

--- 06_homework/hash_table.py
+++ 06_homework/hash_table.py
@@ -1,44 +1,50 @@
-storage = [0 for x in range(15)]
+codes_storage = [0 for x in range(15)]
+strings_storage = ['' for x in range(15)]
 
 
 def hash(key):
-    index = sum(list(map(ord, key)))
-    index = sum(list(map(int, str(index))))
-    return index % len(storage)
+    return len(key) % len(codes_storage)
 
 
-def get_element(key):
-    index = hash(key)
-    return storage[index]
+def rehash(old_hash):
+    return (old_hash + 1) % len(codes_storage)
 
 
 def set_value(key, value):
     index = hash(key)
-    storage[index] = value
+    while codes_storage[index]:
+        index = rehash(index)
+    codes_storage[index] = value
+    strings_storage[index] = key
+
+
+def get_element(key):
+    return codes_storage[strings_storage.index(key)]
 
 
 def reset_value(key):
-    index = hash(key)
-    storage[index] = 0
+    codes_storage[strings_storage.index(key)] = 0
+    strings_storage[strings_storage.index(key)] = ''
 
 
-print(storage)
+print(codes_storage)
+print(strings_storage)
 
-set_value('abc', 171)
-set_value('xyz', 1)
-set_value('bxac', 14)
-set_value('Cvposdf[]Fd', 57)
-set_value('-a-Df-X', 143)
+set_value('abc', 1)
+set_value('bca', 6)
+set_value('ba', 7)
+set_value('bc', 71)
 
 print(get_element('abc'))
-print(get_element('xyz'))
-print(get_element('bxac'))
-print(get_element('Cvposdf[]Fd'))
-print(get_element('-a-Df-X'))
+print(get_element('bca'))
+print(get_element('ba'))
+print(get_element('bc'))
 
-print(storage)
+print(codes_storage)
+print(strings_storage)
 
-reset_value('bxac')
-reset_value('-a-Df-X')
+reset_value('ba')
+reset_value('bc')
 
-print(storage)
+print(codes_storage)
+print(strings_storage)
