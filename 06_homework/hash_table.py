4922368aec9f85dc3f8197f9632f0af0108afb49
BreslavetsAlexander <46530223+BreslavetsAlexander@users.noreply.github.com>
2019-11-03 15:33:58 +0300

    Update hash_table.py

--- 06_homework/hash_table.py
+++ 06_homework/hash_table.py
@@ -1,50 +1,58 @@
-codes_storage = [0 for x in range(15)]
-strings_storage = ['' for x in range(15)]
+values_storage = [0 for x in range(15)]
+keys_storage = ['' for x in range(15)]
 
 
 def hash(key):
-    return len(key) % len(codes_storage)
+    return len(key) % len(values_storage)
 
 
 def rehash(old_hash):
-    return (old_hash + 1) % len(codes_storage)
+    return (old_hash + 1) % len(values_storage)
 
 
 def set_value(key, value):
     index = hash(key)
-    while codes_storage[index]:
+    while values_storage[index]:
         index = rehash(index)
-    codes_storage[index] = value
-    strings_storage[index] = key
+    values_storage[index] = value
+    keys_storage[index] = key
+    new_index = keys_storage.index(key)
+    if index != new_index:
+        values_storage[new_index] = value
+        reset_value(key)
 
 
 def get_element(key):
-    return codes_storage[strings_storage.index(key)]
+    return values_storage[keys_storage.index(key)]
 
 
 def reset_value(key):
-    codes_storage[strings_storage.index(key)] = 0
-    strings_storage[strings_storage.index(key)] = ''
+    index = keys_storage.index(key)
+    values_storage[index] = 0
+    keys_storage[index] = ''
 
 
-print(codes_storage)
-print(strings_storage)
+print(values_storage)
+print(keys_storage)
 
 set_value('abc', 1)
-set_value('bca', 6)
+set_value('abc', 5)
+set_value('aba', 2)
+set_value('aba', 10)
 set_value('ba', 7)
+set_value('ba', 17)
 set_value('bc', 71)
 
 print(get_element('abc'))
-print(get_element('bca'))
+print(get_element('aba'))
 print(get_element('ba'))
 print(get_element('bc'))
 
-print(codes_storage)
-print(strings_storage)
+print(values_storage)
+print(keys_storage)
 
 reset_value('ba')
 reset_value('bc')
 
-print(codes_storage)
-print(strings_storage)
+print(values_storage)
+print(keys_storage)
