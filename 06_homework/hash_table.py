65d76121a8c7c80267134d557d610a72ed02b033
BreslavetsAlexander <46530223+BreslavetsAlexander@users.noreply.github.com>
2019-11-03 17:31:18 +0300

    Update hash_table.py

--- 06_homework/hash_table.py
+++ 06_homework/hash_table.py
@@ -1,35 +1,38 @@
-values_storage = [0 for x in range(15)]
-keys_storage = ['' for x in range(15)]
+values_storage = [[] for x in range(15)]
+keys_storage = [[] for x in range(15)]
 
 
 def hash(key):
-    return len(key) % len(values_storage)
-
-
-def rehash(old_hash):
-    return (old_hash + 1) % len(values_storage)
+    index = sum(list(map(ord, key)))
+    index = sum(list(map(int, str(index))))
+    return index % len(values_storage)
 
 
 def set_value(key, value):
     index = hash(key)
-    while values_storage[index]:
-        index = rehash(index)
-    values_storage[index] = value
-    keys_storage[index] = key
-    new_index = keys_storage.index(key)
-    if index != new_index:
-        values_storage[new_index] = value
-        reset_value(key)
+    values_storage[index].append(value)
+    keys_storage[index].append(key)
 
 
 def get_element(key):
-    return values_storage[keys_storage.index(key)]
+    index = hash(key)
+    j = ''
+    for i in range(len(keys_storage[index])):
+        if len(keys_storage[index]):
+            if keys_storage[index][i] == key:
+                j = i
+    return values_storage[index][j]
 
 
 def reset_value(key):
-    index = keys_storage.index(key)
-    values_storage[index] = 0
-    keys_storage[index] = ''
+    index = hash(key)
+    j = ''
+    for i in range(len(keys_storage[index])):
+        if len(keys_storage[index]):
+            if keys_storage[index][i] == key:
+                j = i
+                values_storage[index][j] = 0
+                keys_storage[index][j] = ''
 
 
 print(values_storage)
@@ -37,22 +40,25 @@ print(keys_storage)
 
 set_value('abc', 1)
 set_value('abc', 5)
-set_value('aba', 2)
-set_value('aba', 10)
-set_value('ba', 7)
-set_value('ba', 17)
-set_value('bc', 71)
-
-print(get_element('abc'))
-print(get_element('aba'))
-print(get_element('ba'))
-print(get_element('bc'))
+set_value('bac', 55)
+set_value('bac', 75)
+set_value('aba', 54)
+set_value('aba', 84)
+set_value('abad', 814)
 
 print(values_storage)
 print(keys_storage)
 
-reset_value('ba')
-reset_value('bc')
+print(get_element('abc'))
+print(get_element('bac'))
+print(get_element('aba'))
+print(get_element('abad'))
+
+print(values_storage)
+print(keys_storage)
+
+reset_value('abc')
+reset_value('bac')
 
 print(values_storage)
 print(keys_storage)
