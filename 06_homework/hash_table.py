cd680fcc7010cff302f6919397f2d6aac9c086be
BreslavetsAlexander <46530223+BreslavetsAlexander@users.noreply.github.com>
2019-11-03 20:08:40 +0300

    Update hash_table.py

--- 06_homework/hash_table.py
+++ 06_homework/hash_table.py
@@ -10,18 +10,15 @@ def hash(key):
 
 def set_value(key, value):
     index = hash(key)
-    values_storage[index].append(value)
-    keys_storage[index].append(key)
+    if keys_storage[index].count(key) < 1:
+        keys_storage[index].append(key)
+        values_storage[index].append(value)
+    values_storage[index][-1] = value
 
 
 def get_element(key):
     index = hash(key)
-    j = ''
-    for i in range(len(keys_storage[index])):
-        if len(keys_storage[index]):
-            if keys_storage[index][i] == key:
-                j = i
-    return values_storage[index][j]
+    return values_storage[index][-1]
 
 
 def reset_value(key):
@@ -35,30 +32,19 @@ def reset_value(key):
                 keys_storage[index][j] = ''
 
 
-print(values_storage)
-print(keys_storage)
-
 set_value('abc', 1)
-set_value('abc', 5)
-set_value('bac', 55)
-set_value('bac', 75)
-set_value('aba', 54)
-set_value('aba', 84)
-set_value('abad', 814)
-
-print(values_storage)
-print(keys_storage)
+set_value('abc', 2)
+set_value('abc', 22)
+set_value('abd', 225)
+set_value('abd', 220)
 
 print(get_element('abc'))
-print(get_element('bac'))
-print(get_element('aba'))
-print(get_element('abad'))
+print(get_element('abd'))
 
 print(values_storage)
 print(keys_storage)
 
 reset_value('abc')
-reset_value('bac')
 
 print(values_storage)
 print(keys_storage)
