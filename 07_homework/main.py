9b66c2964a3a521098a3b439fef5d7431a0f90b2
Breslavets Alexander <46530223+BreslavetsAlexander@users.noreply.github.com>
2020-02-09 14:36:30 +0300

    Update main.py

--- 07_homework/main.py
+++ 07_homework/main.py
@@ -1,7 +1,7 @@
 from Fraction import Fraction
 from IrreducibleFraction import IrreducibleFraction
 
-fraction = Fraction(12, 80)
+fraction = Fraction()
 irreducible_fraction = IrreducibleFraction(34, 120)
 print(fraction)
 print(irreducible_fraction)
