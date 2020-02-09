fd2352777e9e1f7d976bd55199833c50c95ec156
Breslavets Alexander <46530223+BreslavetsAlexander@users.noreply.github.com>
2020-02-09 14:36:02 +0300

    Update Fraction.py

--- 07_homework/Fraction.py
+++ 07_homework/Fraction.py
@@ -1,9 +1,7 @@
 class Fraction:
-    def __init__(self, numerator=None, denominator=None):
+    def __init__(self, numerator=0, denominator=1):
         self.numerator = numerator
         self.denominator = denominator
-        if not self.numerator or not self.denominator:
-            self.input_fraction()
 
     def input_fraction(self):
         self.fraction = input('Type a fraction: ')
@@ -11,20 +9,16 @@ class Fraction:
         self.numerator, self.denominator = int(self.numerator), int(self.denominator)
 
     def reduce(self):
-        numerator_dividers = self.find_dividers(self.numerator)
-        denominator_dividers = self.find_dividers(self.denominator)
-        common_dividers = list(numerator_dividers & denominator_dividers)
-        for i in common_dividers[::-1]:
-            if not self.numerator % i and not self.denominator % i:
-                self.numerator //= i
-                self.denominator //= i
-
-    def find_dividers(self, num):
-        dividers = set()
-        for i in range(2, num):
-            if not num % i:
-                dividers.add(i)
-        return dividers
+        a = self.numerator
+        b = self.denominator
+        while a and b:
+            if a > b:
+                a = a % b
+            else:
+                b = b % a
+        nod = a + b
+        self.numerator //= nod
+        self.denominator //= nod
 
     def __str__(self):
         return f'{self.numerator}/{self.denominator}'
