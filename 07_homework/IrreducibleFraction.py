4c9794b88b89066fd90a9ec56a718642a0302359
Breslavets Alexander <46530223+BreslavetsAlexander@users.noreply.github.com>
2020-02-14 21:49:37 +0300

    Update IrreducibleFraction.py

--- 07_homework/IrreducibleFraction.py
+++ 07_homework/IrreducibleFraction.py
@@ -4,4 +4,4 @@ from Fraction import Fraction
 class IrreducibleFraction(Fraction):
     def __init__(self, numerator=None, denominator=None):
         super().__init__(numerator, denominator)
-        super().reduce()
+        self.reduce()
